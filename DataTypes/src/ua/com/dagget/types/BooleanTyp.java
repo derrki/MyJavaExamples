64538dcf1ab784700fe4672412f90f64963c7e65
Roman Kaminski <derrki@gmaol.com>
2017-09-18 00:55:21 +0300

    Add  BooleanTyp class

---
+++ DataTypes/src/ua/com/dagget/types/BooleanTyp.java
@@ -0,0 +1,46 @@
+package ua.com.dagget.types;
+
+/*
+  Java boolean Example
+  This Java Example shows how to declare and use Java primitive boolean variable
+  inside a java class.
+*/
+public class BooleanTyp {
+    /*
+     * boolean is simple Java type which can have only of two values; true or false. - boolean - це простий тип Java, який може мати лише два значення; Правда чи неправда.
+     * All rational expressions retrun this type of value. - Всі раціональні вирази повертають цей тип значення.
+     *
+     * Declare boolean varibale as below - Оголосіть булеву змінну, як показано нижче
+     *
+     * boolean <variable name> = <default value>; - <ім'я змінної> = <значення за замовчуванням>;
+     *
+     * here assigning default value is optional. - тут призначення значення за замовчуванням є необов'язковим.
+     */
+
+
+
+    boolean b1 = true;
+    boolean b2 = false;
+    boolean b3 = 2 > 3;
+
+    static void testSyntax (int a, int b){
+        //можна написати ((a<10) && (b > 10)), але логічні оператори мають
+        //менший пріоритет порівняно з операторами порівняння. Крім унарного !
+        //все залежить від зручності читання
+        if (a < 10 && b > 10){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+
+    static void testOr(int a, int b){
+        if (a < 5 || b > 5){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+}
--- DataTypes/src/ua/com/dagget/types/MainTypes.java
+++ DataTypes/src/ua/com/dagget/types/MainTypes.java
@@ -1,12 +1,18 @@
 package ua.com.dagget.types;
 
-/**
- * Created by press on 17.09.2017.
- */
 public class MainTypes {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("Hello");
-        System.out.println("Hello World");
+
+
+        int a = 2, b = 8;
+
+        BooleanTyp booleanTyp = new BooleanTyp();
+
+
+        BooleanTyp.testSyntax(a, b);
+        BooleanTyp.testOr(a, b);
+//        System.out.println(booleanTyp.b1);
+//        System.out.println(booleanTyp.b2);
+//        System.out.println(booleanTyp.b3);
     }
 }

b3d44d1bafda050d1b3cc0aa6475578722c68073
Roman Kaminskyy <derrki@gmail.com>
2017-09-17 21:04:02 +0300

    Add first class

---
+++ DataTypes/src/ua/com/dagget/types/MainTypes.java
@@ -0,0 +1,10 @@
+package ua.com.dagget.types;
+
+/**
+ * Created by press on 17.09.2017.
+ */
+public class MainTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}

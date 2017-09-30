a4573725af1b1924fd9bb4f1c763971c51174d0e
Roman Kaminski <derrki@gmaol.com>
2017-10-01 02:55:33 +0300

    Add new code in classes WhiteCrow, WhiteCrowTest1

--- Juja/src/ua/com/juja/week2/algoritthm/WhiteCrow.java
+++ Juja/src/ua/com/juja/week2/algoritthm/WhiteCrow.java
@@ -4,7 +4,31 @@ package ua.com.juja.week2.algoritthm;
  * 1. Make it work. 2. Make it right. 3. Make it fast
  */
 public class WhiteCrow {
-    public static int find(int[] nums) {
-        return 0;
+
+    public static final int ERROR_CODE = -1;
+
+    public static int find(int[] array) {
+
+        for (int first = 0; first < array.length; first++) {
+            if (!found(array, first)){
+                return first;
+            }
+        }
+        return ERROR_CODE;
+    }
+
+    private static boolean found(int[] array, int givenNumbPosition) {
+        int foundNumber = array[givenNumbPosition];
+
+        for (int index = 0; index < array.length; index++) {
+
+            if (givenNumbPosition == index){
+                continue;
+            }
+            if (array[index] == foundNumber) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Juja/src/ua/com/juja/week2/algoritthm/WhiteCrowTest2.java
+++ Juja/src/ua/com/juja/week2/algoritthm/WhiteCrowTest2.java
@@ -1,26 +1,19 @@
 package ua.com.juja.week2.algoritthm;
 
-import java.util.Date;
-
 /**
  * 1. Make it work. 2. Make it right. 3. Make it fast
  */
-public class WhiteCrowTest1 {
+public class WhiteCrowTest2 {
     public static void main(String[] args) {
-        long start = new Date().getTime();
-
-        int[] nums = {1, 1, 2, 2, 3, 3, 4};
+        int[] nums = {1, 2};
         int position = WhiteCrow.find(nums);
 
-        int expected = 6;
+        int expected = -1;
         if (position == expected) {
             System.out.println("OK");
         } else {
             System.out.println("Was: " + position + " but expected: " + expected);
             System.out.println("FAIL");
         }
-
-        long now = new Date().getTime();
-        System.out.println("Time in mills: " + (now -start));
     }
 }

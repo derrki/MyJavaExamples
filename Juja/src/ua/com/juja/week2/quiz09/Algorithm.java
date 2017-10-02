ed3ddec7f5a1704f2eacb10248c7e9dd4a0f2a49
Roman Kaminski <derrki@gmaol.com>
2017-10-02 23:36:20 +0300

    add new mehtod in class Algorithm

--- Juja/src/ua/com/juja/week2/quiz09/Algorithm.java
+++ Juja/src/ua/com/juja/week2/quiz09/Algorithm.java
@@ -1,8 +1,11 @@
 package ua.com.juja.week2.quiz09;
 
+import java.util.Arrays;
+
 public class Algorithm {
 
-    static int[] exchangeOfElements (int [] array, int i, int j){
+    //1
+    static int[] exchangeOfElements(int[] array, int i, int j) {
 
         int tmp = array[i];
         array[i] = array[j];
@@ -10,21 +13,23 @@ public class Algorithm {
         return array;
     }
 
-    static  int [] invertArray(int [] array){
+    //2
+    static int[] invertArray(int[] array) {
 
-        for (int k = 0; k < array.length/2; k++) {
+        for (int k = 0; k < array.length / 2; k++) {
             int tmp = array[k];
-            array[k] = array[array.length - 1 -k];
-            array[array.length - 1 -k] = tmp;
+            array[k] = array[array.length - 1 - k];
+            array[array.length - 1 - k] = tmp;
         }
 
         return array;
     }
 
-    static int linearSearch (int [] array, int x){
+    //3
+    static int linearSearch(int[] array, int x) {
 
-        for (int j = 0; j <  array.length; j++) {
-            if (array[j] == x){
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] == x) {
                 return j;
             }
 
@@ -32,21 +37,93 @@ public class Algorithm {
         return -1;
     }
 
-   static int f(int [] array, int x){
+    //4
+    static int f(int[] array, int x) {
         int low = 0;
-        int high = array.length -1;
+        int high = array.length - 1;
 
         while (low <= high) {
             int mid = (low + high) >>> 1;
             int midVal = array[mid];
-            if(midVal < x)
-                low = mid +1;
-            else  if (midVal > x)
+            if (midVal < x)
+                low = mid + 1;
+            else if (midVal > x)
                 high = mid - 1;
             else
                 return mid;
         }
 
         return -(low + 1);
+    }
+
+    //5
+    static int[] f2(int[] arr) {
+        for (int barrier = arr.length - 1; barrier >= 0; barrier--) {
+            for (int index = 0; index < barrier; index++) {
+                if (arr[index] > arr[index + 1]) {
+                    int tmp = arr[index];
+                    arr[index] = arr[index + 1];
+                    arr[index + 1] = tmp;
+                    System.out.println(Arrays.toString(arr));
+                }
+
+            }
+
+        }
+        return arr;
+    }
+
+    //6
+    static int[] f3(int[] arr) {
+        for (int barrier = 1; barrier < arr.length; barrier++) {
+            int newElement = arr[barrier];
+            int location = barrier - 1;
+            while (location >= 0 && arr[location] > newElement) {
+                arr[location + 1] = arr[location];
+                location--;
+                System.out.println(Arrays.toString(arr));
+            }
+            arr[location + 1] = newElement;
+        }
+        return arr;
+    }
+
+    //7
+    static int[] f4(int[] array) {
+        for (int barrier = 0; barrier < array.length - 1; barrier++) {
+            for (int index = barrier + 1; index < array.length; index++) {
+                if (array[barrier] > array[index]) {
+                    int tmp = array[index];
+                    array[index] = array[barrier];
+                    array[barrier] = tmp;
+                    System.out.println(Arrays.toString(array));
+                }
+            }
+        }
+        return array;
+    }
+
+    //8
+    static int[] f5(int[] array) {
+        for (int index = 0; index < array.length - 1; index++) {
+            int tmp = array[index];
+            array[index] = array[index + 1];
+            array[index + 1] = tmp;
+            System.out.println(Arrays.toString(array));
+        }
+        return array;
+    }
+
+    //9
+    static int[] f6(int[] array) {
+        for (int index = 0; index < array.length - 1; index++) {
+            if (array[index] > array[index + 1]) {
+                int tmp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = tmp;
+                System.out.println(Arrays.toString(array));
+            }
+        }
+        return array;
     }
 }
--- Juja/src/ua/com/juja/week2/quiz09/Main.java
+++ Juja/src/ua/com/juja/week2/quiz09/Main.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int [] array = {6,8,7,5,1,-2,65,48};
+        int [] array = {6,8,14,5,1,-2,65,48};
+        int [] array2 = {6,8,14,5,1,-2,65,48};
+        int [] array3 = {6,8,14,5,1,-2,65,48};
 
         System.out.println("Algorithm 1 ----------------");
         array = Algorithm.exchangeOfElements(array, 0, array.length-1);
@@ -25,6 +27,28 @@ public class Main {
         System.out.println("Algorithm 4 ----------------");
         int ind = Algorithm.f(array, 7);
         System.out.println(ind);
+
+        System.out.println("Algorithm 5 ----------------");
+        int [] inde = Algorithm.f2(array);
+        System.out.println(inde);
+
+        System.out.println("Algorithm 6 ----------------");
+        int [] in = Algorithm.f3(array2);
+        System.out.println(in);
+
+        System.out.println("Algorithm 7 ----------------");
+        int [] indexe = Algorithm.f4(array3);
+        System.out.println(indexe);
+
+        System.out.println("Algorithm 8 ----------------");
+        int [] indexes = Algorithm.f5(array);
+        System.out.println(indexes);
+
+        System.out.println("Algorithm 9 ----------------");
+        int [] indexesys = Algorithm.f6(array);
+        System.out.println(indexesys);
+
+
 
     }
 }
